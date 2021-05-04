6a5aeeca4eea14d431e6005ba89e6e3a9c0ca979
luisVillegas <44413936+luicho98@users.noreply.github.com>
2021-05-04 04:16:54 -0600

    Preparado espacio de imagenes

---
+++ src/components/Inspeccion/Contenedor.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import Cuadricula from './Cuadricula';
+import Modal from './Modal';
+
+class Contenedor extends Component {
+    constructor() {
+          super();
+          this.state = {
+                selectedImg :null
+        };
+    }
+
+render(){
+
+  return (
+    <div className="App">
+      <Cuadricula selectedImg={this.selectedImg} />
+      { this.selectedImg && (
+        <Modal selectedImg={this.selectedImg} />
+      )}
+    </div>
+  );
+      
+}
+}
+
+export default Contenedor;
