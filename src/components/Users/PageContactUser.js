afb06470b18725df946cc51ed7355a3ea83ede33
luisVillegas <44413936+luicho98@users.noreply.github.com>
2021-05-07 04:47:12 -0600

    pagina datos de usuario

---
+++ src/components/Users/PageContactUser.js
@@ -0,0 +1,135 @@
+import React, {Component} from "react";
+import "@fortawesome/fontawesome-free";
+import Header from "../Header/header";
+import Footer from "../Footer/footer";
+export default class PatInspecR extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+
+        <PageList />
+
+        <Footer />
+      </div>
+    );
+  }
+}
+
+class PageList extends Component {
+    constructor(props) {
+    super(props);
+    this.state = {
+        //viene de contribuyente
+      Nombre_Solicitante: "",
+      Correo: "",
+      Telefono1: ""
+    };
+    //this.onClick = this.handleClick.bind(this);
+    //this.handleInputChange = this.handleInputChange.bind(this);
+  }
+  componentDidMount() {
+    const {id} = this.props;
+    this.solicitarDatosporCodigo(id);
+  }
+  solicitarDatosporCodigo(cod) {
+    fetch("http://localhost:3001/api/EspForm/selected", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        /*'Access-Control-Allow-Headers': '*',
+				"Access-Control-Allow-Origin": "*",
+      			"Access-Control-Allow-Credentials": "true",
+      			"Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT"*/
+      },
+      body: JSON.stringify({codigo: cod}),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        //console.log("imprimiendo datos ", data[0]);
+        //this.setState({datos: data[0]});
+        this.getDatos(data[0]);
+      });
+  }
+  /*
+  toggleChange = () => {
+    this.setState({
+      isChecked: !this.state.isChecked,
+    });
+  };*/
+
+  getDatos(datos) {
+
+    this.setState({ 
+      Nombre_Solicitante: datos.Nombre,
+    });
+    this.setState({ 
+      Correo: datos.Correo,
+    });
+    this.setState({ 
+      Telefono1: datos.Telefono,
+    });
+
+    console.log(datos);
+  }
+
+  render() {
+      const {
+      Nombre_Solicitante,
+      Correo,
+      Telefono1
+    } = this.state;
+    return (
+        <>
+        <div className="row">
+            <div className="col-sm-12 col-sm-offset-0">
+                <div className="card">
+                    <div className=" card-header form-group text-center">
+                        <h1>
+                            <strong>
+                                Datos de usuario solicitante
+                            </strong>
+                        </h1>
+                    </div>
+                    <div className="card-body">
+                        <div className="form-group col-md-10">
+                            <h7>
+                                <div className="form-group col-md-4">
+                                    <label htmlFor="FK_Solicitud_Patente">
+                                        Nombre:{Nombre_Solicitante}
+                                    </label>
+                                </div>
+                            </h7>
+                            <h7>
+                                <div className="form-group col-md-4">
+                                    <label htmlFor="FK_Solicitud_Patente">
+                                        Telefono:{Telefono1}
+                                    </label>
+                                </div>
+                            </h7>
+                            <h7>
+                                <div className="form-group col-md-4">
+                                    <label htmlFor="FK_Solicitud_Patente">
+                                        Correo:{Correo}
+                                    </label>
+                                </div>
+                            </h7>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="form-group col-md-4">
+            <a href="./PatInspecR">
+              <button
+                className="btn btn-primary text-center"
+              >
+                Guardar
+              </button>
+            </a>
+        </div>
+        </>
+    );
+  }
+}
